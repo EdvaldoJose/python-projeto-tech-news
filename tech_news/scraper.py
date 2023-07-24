b1674c5750880f4e5990b58ece984ca6c37b4a43
Edvaldo José <edvaldojose003@gmail.com>
2023-07-24 15:56:17 -0300

    feat: 3 - Crie a função scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -28,7 +28,9 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page_link = selector.css(".nav-links .next::attr(href)").get()
+    return next_page_link
     raise NotImplementedError
 
 
