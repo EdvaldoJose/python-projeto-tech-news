0487186e2b7492dbbabc604be469d6007567ee52
Edvaldo José <edvaldojose003@gmail.com>
2023-07-24 11:41:57 -0300

    feat: 1 - Crie a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,20 @@
 # Requisito 1
+import requests
+import time
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # raise NotImplementedError
+    try:
+        time.sleep(1)
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
+        if response.status_code != 200:
+            return None
+        return response.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
@@ -26,4 +39,3 @@ def scrape_news(html_content):
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
     raise NotImplementedError
-    pass
