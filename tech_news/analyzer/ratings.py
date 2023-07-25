cab9abd644473b8387a8aed681f92208eaf7c21e
Edvaldo José <edvaldojose003@gmail.com>
2023-07-25 20:36:37 -0300

    feat: 10 - Crie a função top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,19 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories_counter = {}
+
+    for cat_news in db.news.find():
+        category = cat_news.get("category")
+        if category:
+            categories_counter[category] = categories_counter.get(
+                category, 0) + 1
+
+    top_categories = sorted(
+        categories_counter.items(), key=lambda item: (-item[1], item[0])
+    )
+
+    return [category for category, _cat_news in top_categories[:5]]
     raise NotImplementedError
